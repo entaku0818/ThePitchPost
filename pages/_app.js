3c149944de75c7cd681bb8374c560732ddf5e317
entaku <entaku19890818@gmail.com>
2023-04-23 20:46:40 +0900

    add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,43 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
+  const router = useRouter();
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-
+    const handleRouterChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
 
-  return <Component {...pageProps} />
+
+  return (
+      <>
+        <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_TRACKING_ID}');
+           `,
+            }}
+        />
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
