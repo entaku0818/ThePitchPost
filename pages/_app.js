bca2f978d89d3f81477e63bbd5822ac966868292
entaku <entaku19890818@gmail.com>
2023-05-02 16:29:44 +0900

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,9 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
-import Script from "next/script";
+
 import '../styles/globals.css';
 import GoogleAnalytics from "../components/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouterChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouterChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouterChange);
-    };
-  }, [router.events]);
-
+    
 
   return (
       <>
--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,10 @@
 import Document, { Head, Main, NextScript } from 'next/document'
-import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
     render() {
         return (
             <html>
             <Head>
-                {/* Google Analytics */}
-                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_TRACKING_ID}');
-              `,
-                    }}
-                />
             </Head>
             <body>
             <Main />
