e509df10b60c702c708faf50b9b7918e546317f8
entaku <entaku19890818@gmail.com>
2023-05-02 16:21:10 +0900

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
 import Script from "next/script";
 import '../styles/globals.css';
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -19,23 +20,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
       <>
-        <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        />
-        <Script
-            id="gtag-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
- 
-           gtag('config', '${gtag.GA_TRACKING_ID}');
-           `,
-            }}
-        />
+          <GoogleAnalytics />
         <Component {...pageProps} />
       </>
   );
