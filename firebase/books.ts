d50dbfe151f6513dd419f220f5d46fc9d6d2cddb
entaku <entaku19890818@gmail.com>
2023-05-30 09:28:44 +0900

    viewの表示追加

--- firebase/books.ts
+++ firebase/books.ts
@@ -1,4 +1,4 @@
-import {collection, getDocs, getFirestore, limit, query} from 'firebase/firestore'
+import {collection, getDocs, getFirestore, limit, query, updateDoc} from 'firebase/firestore'
 import {db} from '../firebase/firebase' // Initialize FirebaseApp
 import { doc, getDoc } from 'firebase/firestore';
 
@@ -20,13 +20,27 @@ export async function getBooks(): Promise<Book[]> {
 
     booksSnapshot.forEach((doc) => {
         const book = doc.data() as Book
+        const bookId = doc.id
+
+        updateListViewCount(bookId)
+
         books.push({ ...book, id: doc.id })
     })
 
     return books
 }
 
+async function updateListViewCount(bookId: string): Promise<void> {
+    const bookRef = doc(db, 'books', bookId)
+    const bookDoc = await getDoc(bookRef)
 
+    if (bookDoc.exists()) {
+        const currentViews = bookDoc.data().listViews || 0
+        const newViews = currentViews + 1
+
+        await updateDoc(bookRef, { listViews: newViews })
+    }
+}
 
 export async function getBook(id: string): Promise<Book | null> {
     if (!id) {
@@ -37,6 +51,9 @@ export async function getBook(id: string): Promise<Book | null> {
 
     if (bookDoc.exists()) {
         const book = bookDoc.data() as Book;
+        const currentViews = bookDoc.data().detailViews || 0
+        const newViews = currentViews + 1
+        await updateDoc(bookRef, { detailViews: newViews })
         return { ...book, id: bookDoc.id };
     } else {
         return null;
